6a92181a9c15f9cfac17de1aa7e7f26fe4425afe
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-07-01 23:51:05 +0530

    Component: Handle user in sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,17 +1,34 @@
-import { Paper,Avatar, List, ListItem, ListItemText } from '@mui/material'
-import React from 'react'
+import { Paper, Avatar, List, ListItem, ListItemText } from '@mui/material'
+import { collection, getDocs } from 'firebase/firestore'
+import React, { useEffect } from 'react'
+import { database } from '../firebase/setup'
 
 const Sidebar = () => {
+
+  const getUser = async () => {
+    const userRef=collection(database,"Users")
+    try {
+      const data=await getDocs(userRef)
+      const filteredData = data.docs
+      console.log(filteredData);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(()=>{getUser()})
+
   return (
     <div>
-        <Paper elevation={0} sx={{border:"1px solid #D4D4D4"}}>
-            <List>
-                <ListItem>
-                    <Avatar/>
-                    <ListItemText/>
-                </ListItem>
-            </List>
-        </Paper>
+      <Paper elevation={0} sx={{ border: "1px solid #D4D4D4" }}>
+        <List>
+          <ListItem>
+            <Avatar />
+            <ListItemText />
+          </ListItem>
+        </List>
+      </Paper>
     </div>
   )
 }
