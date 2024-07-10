df5d5f2530eceb89b5c5edcac08d1b0eb946dd70
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-07-11 00:09:31 +0530

    Sidebar Comp: Create a search bar to find users from chat lists

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import Header from './Components/Header'
 import LoginSignUpPage from './Components/LoginSignupPage'
 import SignIn from './Components/SignIn'
 import Main from './Components/Main'
-import Sidebar from './Components/Sidebar'
 
 function App() {
 
   return (
     <>
-    <Sidebar/>
     {/* <Header/> */}
     {/* <SignIn/> */}
     {/* <LoginSignUpPage/> */}
-    {/* <Main/> */}
+    <Main/>
     </>
   )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,11 +2,13 @@ import { Paper, Avatar, List, ListItem, ListItemText } from '@mui/material'
 import { collection, getDocs } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { database,auth } from '../firebase/setup'
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import "./Sidebar.css"
+
 
 
 const Sidebar = () => {
 
-  console.log("hi");
   const [users, setUsers] = useState([])
 
 
@@ -32,8 +34,13 @@ const Sidebar = () => {
 
 
   //todo[DONE]: add filter function so that the current user  does not shows up 
+
   return (
     <div>
+      <div className='search'>
+      <FaMagnifyingGlass className='search-icon'/>
+      <input className='search-field' placeholder='Search for new chat'/>
+      </div>
       {users.filter(user => user.id !== auth.currentUser?.uid).map((user) => {
         return <>
           <Paper elevation={0} sx={{ border: "1px solid #D4D4D4" }}>
