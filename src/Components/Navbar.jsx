6989a613a39ad89dbdadb101af0956fd91c91c57
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-07-19 19:35:46 +0530

    Drop Custom Css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,29 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import { Avatar } from '@mui/material';
+import React from 'react';
 import { auth } from '../firebase/setup';
-import "./Navbar.css"
 
 export default function Navbar(props) {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar elevation={0} sx={{ backgroundColor: "#A855F7", height: "50px" }} position="static">
-        <Toolbar variant="dense">
-          <img className='profile-img' src={props.receiverProfileImg ?? auth.currentUser?.photoURL}></img>
-          {/* TODO: on reload the profile picture does not shows up */}
-          <h3 className='receiver-name'>{props.receiverUsername}</h3>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <nav className="bg-gray-100 shadow-md">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-12">
+                    <div className="flex-shrink-0 flex items-center">
+                        <img
+                            className="h-8 w-8 rounded-full object-cover"
+                            src={props.receiverProfileImg ?? auth.currentUser?.photoURL}
+                            alt="Profile"
+                        />
+                        <h3 className="ml-3 font-medium text-gray-800 text-sm">
+                            {auth.currentUser.displayName ?? props.receiverUsername}
+                        </h3>
+                    </div>
+                    <div className="absolute inset-x-0 flex justify-center">
+                        <h1 className="text-3xl font-bold text-[#002D74] cursor-pointer" onClick={`/Main`} >Chatty
+                        </h1>
+                    </div>
+                    <div className="flex-shrink-0 w-24"> {/* This is a placeholder to balance the layout */}
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -10,64 +10,69 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
 
-  const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
 
-  const getUser = async () => {
-    const userRef = collection(database, "Users")
-    try {
-      const data = await getDocs(userRef)
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id
-      }))
-      // console.log(filteredData);
-      setUsers(filteredData);
-    } catch (error) {
-      console.error(error);
+    const getUser = async () => {
+        const userRef = collection(database, "Users")
+        try {
+            const data = await getDocs(userRef)
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id
+            }))
+            // console.log(filteredData);
+            setUsers(filteredData);
+        } catch (error) {
+            console.error(error);
+        }
+
     }
 
-  }
-
-  //TODO[Done]: is this component stuck in a loop? find out why it repaints multiple times until firebase quota exceeds.
-  //This was happening because of the useEffect hook continuously updating whenever changes performed over users, setting the dependency array empty fixes the issue for now.
-  // useEffect(() => { getUser() }, [users])
-  useEffect(() => { getUser() }, [])
+    //TODO[Done]: is this component stuck in a loop? find out why it repaints multiple times until firebase quota exceeds.
+    //This was happening because of the useEffect hook continuously updating whenever changes performed over users, setting the dependency array empty fixes the issue for now.
+    // useEffect(() => { getUser() }, [users])
+    useEffect(() => { getUser() }, [])
 
 
-  //todo[DONE]: add filter function so that the current user  does not shows up 
-  // console.log(users);
-  return (
-    <div>
-      <div className='search'>
-        <FaMagnifyingGlass className='search-icon' />
-        <input className='search-field' placeholder='Search for new chat' />
-      </div>
-      {users.filter(user => user.id !== auth.currentUser?.uid).map((user) => {
-        return <>
-          <Link
-          key={user.id}
-            to='/Chat'
-            className='chat-link'
-            state={{
-              id: user.id,
-              username: user.username,
-              profile_image: user.profile_image
-            }} >
-            <Paper elevation={0} sx={{ border: "1px solid #D4D4D4" }}>
-              <List>
-                <ListItem>
-                  <Avatar src={user.profile_image} />
-                  <ListItemText primary={user.username} sx={{ marginLeft: "8px" }} />
-                </ListItem>
-              </List>
-            </Paper>
-          </Link>
-        </>
-      })}
+    //todo[DONE]: add filter function so that the current user  does not shows up 
+    // console.log(users);
+    return (
+        <div className="bg-gray-50 shadow-md h-screen w-96">
+            <div className="flex items-center p-4 border-b border-gray-200">
+                <FaMagnifyingGlass className="text-gray-500 w-5 h-5 ml-6" />
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="ml-2 p-2 w-80 h-8 rounded-xl bg-gray-100 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                />
+            </div>
 
-    </div>
-  )
+            {users.filter(user => user.id !== auth.currentUser?.uid).map((user) => {
+                return <div className='mt-4'>
+                    <Link
+                        key={user.id}
+                        to='/Chat'
+                        className='block px-3 text-gray-700 rounded-lg hover:scale-105 duration-300'
+                        state={{
+                            id: user.id,
+                            username: user.username,
+                            profile_image: user.profile_image
+                        }} >
+                        <Paper elevation={0} sx={{ border: "1px solid #D4D4D4" }}>
+                            <List>
+                                <ListItem>
+                                    <Avatar src={user.profile_image} />
+                                    <ListItemText primary={user.username} sx={{ marginLeft: "8px" }} />
+                                </ListItem>
+                            </List>
+                        </Paper>
+                    </Link>
+                </div>
+            })}
+
+        </div>
+    )
 }
 
 export default Sidebar
