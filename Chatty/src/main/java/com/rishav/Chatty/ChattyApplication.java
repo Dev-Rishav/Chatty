a5dde1bedb4491e078d8526056d6cea80ad09da4
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-03-09 22:33:06 +0530

    Initialize backend with spring boot

---
+++ Chatty/src/main/java/com/rishav/Chatty/ChattyApplication.java
@@ -0,0 +1,13 @@
+package com.rishav.Chatty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattyApplication.class, args);
+	}
+
+}
---
+++ Chatty/src/test/java/com/rishav/Chatty/ChattyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishav.Chatty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
